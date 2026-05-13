744718708be05ec315e3b0bcafb5ee497b967261
bata7 <lopsonovbata762@gmail.com>
2026-05-13 16:12:59 +0800

    Update enable_startup.bat

--- enable_startup.bat
+++ enable_startup.bat
@@ -1,9 +1,14 @@
 @echo off
+set "AHK_FILE=%~dp0VimCapslockArrows.ahk"
+set "STARTUP=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 
-set SCRIPT=%~dp0VimCapslockArrows.ahk
-set STARTUP=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
+if exist "%AHK_FILE%" (
+    powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%STARTUP%\VimCapslockArrows.lnk');$s.TargetPath='%AHK_FILE%';$s.Save()"
+    echo ------------------------------------------------
+    echo Autostart successfully enabled for Vim arrows script. Link: github.com
+    echo ------------------------------------------------
+) else (
+    echo Error: VimCapslockArrows.ahk not found in this folder.
+)
 
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%STARTUP%\VimCapslockArrows.lnk');$s.TargetPath='%SCRIPT%';$s.Save()"
-
-echo Autostart enabled
 pause
