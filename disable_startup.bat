4aa2087ba50930e2c226d8bb7a72a2d4bfbb385e
bata7 <lopsonovbata762@gmail.com>
2026-05-13 16:12:42 +0800

    Update disable_startup.bat

--- disable_startup.bat
+++ disable_startup.bat
@@ -1,8 +1,16 @@
-@echo off
+    @echo off
+set "STARTUP=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 
-set STARTUP=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
+if exist "%STARTUP%\VimCapslockArrows.lnk" (
+    del "%STARTUP%\VimCapslockArrows.lnk"
+)
 
-del "%STARTUP%\VimCapslockArrows.lnk"
+:: На всякий случай удаляем .ahk, если он был скопирован туда вручную вместо ярлыка
+if exist "%STARTUP%\VimCapslockArrows.ahk" (
+    del "%STARTUP%\VimCapslockArrows.ahk"
+)
 
-echo Autostart disabled
+echo ------------------------------------------------
+echo Autostart successfully disabled for Vim arrows script. Link: github.com
+echo ------------------------------------------------
 pause
